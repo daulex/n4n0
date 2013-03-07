8ac164b6202dabe1b25128911d6d2f0bc63f845a
Kirill Galenko <k@bythescruff.com>
2013-03-07 15:58:28 +0000

    now including css and js

--- header.php
+++ header.php
@@ -4,6 +4,13 @@
 	<meta charset="UTF-8">
 	<title><?php wp_title(' | ', true, 'right'); bloginfo('name'); ?></title>
 
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/a/css/reset.css" />
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/a/css/base.css" />
+
+	<?php wp_enqueue_script( "jquery" ); ?>
+	<?php wp_enqueue_script( "plugins", get_template_directory_uri()."/a/js/plugins.js" ); ?>
+	<?php wp_enqueue_script( "site", get_template_directory_uri()."/a/js/site.js" ); ?>
+
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>

b4b143772f0ba952d6902e4f87e618e89d56c7dd
Kirill G <kirillgalenko@gmail.com>
2018-06-25 14:53:26 +0300

    js/css display cache-breaking added

--- header.php
+++ header.php
@@ -1,17 +1,28 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1.0, maximum-scale=1.0">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1.0, maximum-scale=1.0">
 	
-	<title><?php wp_title(' | ', true, 'right'); bloginfo('name'); ?></title>
+  <title><?php wp_title(' | ', true, 'right'); bloginfo('name'); ?></title>
 
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/a/css/base.css" />
 
-	<?php wp_enqueue_script( "jquery" ); ?>
-	<?php wp_enqueue_script( "plugins", get_template_directory_uri()."/a/js/plugins.js" ); ?>
-	<?php wp_enqueue_script( "site", get_template_directory_uri()."/a/js/site.js" ); ?>
 
-	<?php wp_head(); ?>
+  <?php
+    
+    if(current_user_can("administrator")):
+      $v = rand();
+    else:
+      $v = "1";
+    endif;
+
+    wp_enqueue_script( "jquery" );
+    wp_enqueue_script( "plugins", get_template_directory_uri()."/a/js/plugins.js", 0, $v);
+    wp_enqueue_script( "site", get_template_directory_uri()."/a/js/site.js", 0, $v);
+
+  ?>
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/a/css/base.css?v=<?php echo $v; ?>" />
+
+  <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
